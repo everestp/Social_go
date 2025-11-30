44ada6be88c7bbb09aa4033e7a5c7a67bd6e343d
everestp <paudeleverest09@gmail.com>
2025-11-30 19:29:23 +0545

    Env setup

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,6 +1,9 @@
 package env
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 
 func GetString (key ,fallback string ) string {
@@ -9,4 +12,17 @@ func GetString (key ,fallback string ) string {
 		return fallback
 	}
 return  val
+}
+
+func GetInt(key string , fallback int ) int {
+	val , ok :=os.LookupEnv(key)
+	if !ok{
+		return fallback
+	}
+   valAsInt ,err :=   strconv.Atoi(val)
+   if err !=nil {
+	return fallback
+   }
+
+   return  valAsInt
 }
