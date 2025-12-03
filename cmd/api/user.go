a85f554022ebab43fb984ba71ac4cb4100bf8e9a
everestp <paudeleverest09@gmail.com>
2025-12-03 18:24:55 +0545

    48.Adding Followers

--- cmd/api/user.go
+++ cmd/api/user.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/everestp/Social_go/internal/store"
 	"github.com/go-chi/chi/v5"
 )
+
+
+type userKey string
+const userCtx userKey ="user"
 
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse userID from URL
@@ -36,4 +43,78 @@ func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalServerError(w, r, err)
 	}
+}
+  type FollowUser struct {
+	UserID int64 `json:"user_id"`
+
+  }
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+// TODO Revert Back to auth UserID from ctx
+var payload FollowUser
+if err := readJSON(w, r, &payload); err !=nil{
+	app.badRequestResponse(w, r , err )
+}
+  if err := app.store.Followers.Follow(r.Context(), followerUser.ID, payload.UserID); err != nil{
+	app.internalServerError(w, r , err )
+  }
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request){
+unfollowedUser := getUserFromContext(r)
+
+// TODO Revert back to auth UserID from ctx
+ var payload FollowUser
+ if err := readJSON(w, r , &payload); err != nil{
+	app.badRequestResponse(w, r , err )
+	return
+ }
+
+ ctx := r.Context()
+if  err := app.store.Followers.Unfollow(ctx ,unfollowedUser.ID ,payload.UserID); err != nil{
+	app.internalServerError(w, r, err)
+		return
+}
+if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+} 
+
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.badRequestResponse(w, r, fmt.Errorf("invalid post ID"))
+			return
+		}
+
+		user, err := app.store.User.GetByID(r.Context(), id)
+		if err != nil {
+			if errors.Is(err, store.ErrNotFound) {
+				app.notFoundResponse(w, r, err)
+			} else {
+				app.internalServerError(w, r, err)
+				
+
+			}
+			
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+
+
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -7,39 +7,39 @@ import (
 	"time"
 )
 
-
 var (
-	ErrNotFound = errors.New("Resource not found")
-	QueryTimeoutDuration  = time.Second * 5
+	ErrNotFound          = errors.New("resource not found")
+	ErrConflict          = errors.New("resource already exists")
+	QueryTimeoutDuration = time.Second * 5
 )
-
-
-
 type Storage struct {
-	Posts  interface {
-		Create(context.Context  ,*Post) error
-		GetByID(context.Context , int64) (*Post ,error)
-		Delete(context.Context , int64) error 
-		Update(context.Context , *Post) error 
+	Posts interface {
+		Create(context.Context, *Post) error
+		GetByID(context.Context, int64) (*Post, error)
+		Delete(context.Context, int64) error
+		Update(context.Context, *Post) error
 	}
-	User  interface {
-		GetByID(context.Context , int64) (*User ,error)
-		Create(context.Context , *User) error
+	User interface {
+		GetByID(context.Context, int64) (*User, error)
+		Create(context.Context, *User) error
 	}
-	Comments interface{
-			Create(context.Context  ,*Comment) error
-		GetByPostID(ctx context.Context ,postID int64 )([]Comment ,error)
+	Comments interface {
+		Create(context.Context, *Comment) error
+		GetByPostID(ctx context.Context, postID int64) ([]Comment, error)
 	}
-
+	Followers interface {
+		Follow(ctx context.Context, followerID int64, userID int64) error
+		Unfollow(ctx context.Context, followerID int64, userID int64) error
+	}
 }
 
 
-func NewPostgressStorage(db *sql.DB ) Storage {
-	
-	return  Storage{
-         Posts: &PostStore{db},
-		 User:&UserStore{db},
-		 Comments: &CommentStore{db},
+func NewPostgressStorage(db *sql.DB) Storage {
 
+	return Storage{
+		Posts:     &PostStore{db},
+		User:      &UserStore{db},
+		Comments:  &CommentStore{db},
+		Followers: &FollowerStore{db},
 	}
 }
