20339e5a3ceb7f2c91aa32df0e42ee6926a4d83d
everestp <paudeleverest09@gmail.com>
2025-12-04 15:48:40 +0545

    51 .Pagination sorting  52.Feed Filtering

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,15 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/everestp/Social_go/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
-ctx := r.Context()
-feed ,err := app.store.Posts.GetUserFeed(ctx ,int64(12))
-if err != nil {
-	app.internalServerError(w, r , err )
-	return
-}
-if err := app.jsonResponse(w, http.StatusOK, feed) ; err !=nil{
-app.internalServerError(w, r , err )
-}
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+		Tags:   []string{},
+		Search: "",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	
+
+	feed, err := app.store.Posts.GetUserFeed(ctx,int64(12), fq)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
+		app.internalServerError(w, r, err)
+	}
 }
